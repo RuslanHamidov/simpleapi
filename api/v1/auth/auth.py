718b05fd0e5f0afe5b85b68361f276c25ea3f4ce
RuslanHamidov <you@example.comrhamidov202@gmail.com>
2024-04-22 16:47:52 +0000

    initial

---
+++ api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+            require path
+        '''
+        if path is None or excluded_paths is None:
+            return True
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        if path in excluded_paths:
+            return False
+        return True
+    
+    def authorization_header(self, request=None) -> str:
+        '''
+            header
+        '''
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+            current_user
+        """
+        return None
